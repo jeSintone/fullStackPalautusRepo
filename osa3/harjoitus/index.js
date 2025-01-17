bf13fab953efb9cb46e3a13d204dd3f299b90138
jeSinton <sintonej.esse@gmail.com>
2025-01-17 14:34:26 +0200

    baseUrl and cors added

--- osa3/harjoitus/index.js
+++ osa3/harjoitus/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-
-app.use(express.json())
 
 let notes = [
     {
@@ -20,6 +19,22 @@ let notes = [
       important: true
     }
   ]
+
+  const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
+  app.use(express.json())
+  app.use(requestLogger)
+  app.use(cors())
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
 
   const generateId = () => {
     const maxId = notes.length > 0
@@ -72,7 +87,10 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(204).end()
   })
 
-const PORT = 3001
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
