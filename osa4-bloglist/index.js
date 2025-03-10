7b1d601be5aac3480e53998e94c5ebd024f40b00
jeSinton <sintonej.esse@gmail.com>
2025-03-10 15:38:12 +0200

    Osa 4 bloglist tehty 4.2 asti.

--- osa4-bloglist/index.js
+++ osa4-bloglist/index.js
@@ -3,42 +3,20 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+const notesRouter = require('./controllers/blogs')
+app.use('/api/blogs', notesRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
