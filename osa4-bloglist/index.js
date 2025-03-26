6393bba0299e043354256f7a100cb43f4c3dd4db
Jesse Sintonen <sintonej.esse@gmail.com>
2025-03-26 14:45:03 +0200

    split index.js with app.js

--- osa4-bloglist/index.js
+++ osa4-bloglist/index.js
@@ -1,21 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app') // varsinainen Express-sovellus
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-const notesRouter = require('./controllers/blogs')
-app.use('/api/blogs', notesRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
